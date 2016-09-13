a1a405d36045ee9f18c13929da3f23f5153bdbe3
云心随风 <ff1354075450@163.com>
2016-09-14 00:06:59 +0800

    软键盘遮住下方输入问题解决方案

---
+++ ScrollViewKeyBoard/app/src/main/java/edu/basic/scrollviewkeyboard/NextActivity.java
@@ -0,0 +1,15 @@
+package edu.basic.scrollviewkeyboard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ff on 2016/9/13.
+ */
+public class NextActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
